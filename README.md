27920ea7d7bc8c0c9fa46d07ecb7331f81693362
g3rasik <dmitrovnikita2208@gmail.com>
2026-06-09 05:53:41 -0500

    fix xd

---
+++ README.md
@@ -0,0 +1,2 @@
+![CMake Build](https://github.com/vxntvs/lab06/actions/workflows/cmake.yml/badge.svg)
+Git lab 02
---
+++ _builds/default/_3rdParty/Hunter/toolchain/_builds/CMakeFiles/3.31.6/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.12.74+deb13+1-arm64")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.12.74+deb13+1-arm64")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "aarch64")
+
+include("/home/g3rasik/g3rasik/workspace/projects/lab07/tools/polly/default.cmake")
+
+set(CMAKE_SYSTEM "Linux-6.12.74+deb13+1-arm64")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "6.12.74+deb13+1-arm64")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "FALSE")
+
+set(CMAKE_SYSTEM_LOADED 1)
